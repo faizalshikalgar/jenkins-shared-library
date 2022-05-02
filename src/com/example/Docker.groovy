0cd2f45a188cc5f5043fbc5474c40b1461659b83
faizalshikalgar <faizalshikalgar@gmail.com>
2022-05-02 15:33:34 +0530

    add Docker Src package

---
+++ src/com/example/Docker.groovy
@@ -0,0 +1,21 @@
+#!/usr/bin/env groovy
+package com.example
+
+class Docker implements Serializable {
+
+    def  script
+
+    Docker(script){
+        this.script = script
+    }
+
+    def buildDockerImage(String imageName) {
+        script.echo "Building the docker image..."
+        script.withCredentials([script.usernamePassword(credentialsId: 'docker-hubrepo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+            script.sh 'docker build -t faizalshikalgar/demo-app:jma-1.0 .'
+            script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
+            script.sh 'docker push faizalshikalgar/demo-app:jma-1.0'
+        }
+    }
+
+}
--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,7 @@
 #!/usr/bin/env groovy
 
-def call() {
-    echo "Building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hubrepo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t faizalshikalgar/demo-app:jma-1.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push faizalshikalgar/demo-app:jma-1.0'
-    }
+import com.example.Docker
+
+def call(String imageName) {
+    return new Docker(this).buildDockerImage(imageName)
 }
