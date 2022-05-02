2fe82b2dfedbb0bcc845c1e82e03a1c19493c355
faizalshikalgar <85288970+faizalshikalgar@users.noreply.github.com>
2022-05-02 14:27:39 +0530

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
 
-def call() {
+def call(String imageName) {
     echo "Building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hubrepo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t faizalshikalgar/demo-app:jma-1.0 .'
+        sh "docker build -t $imageName ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push faizalshikalgar/demo-app:jma-1.0'
+        sh "docker push $imageName"
     }
 }
